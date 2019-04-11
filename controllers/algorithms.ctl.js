62b7f1eca4cc676c898c6bafd5d2a6cd1843adc7
Adham Khalilieh <adham@megatam.net>
2019-04-12 01:03:33 +0300

    time edit

--- controllers/algorithms.ctl.js
+++ controllers/algorithms.ctl.js
@@ -8,7 +8,12 @@ const { freeAlg } = require('./algs/free-alg');
 module.exports = {
 	getFreeTime: async (req, res, next) => {
 		const { business, services, date_from, date_until } = req.body;
-		const dates = await freeAlg(business, services, new Date(date_from), new Date(date_until), 0);
+		/* create date witth minus 3 hours */
+		//TODO fix the database time to israeli
+		const dateFrom = await new Date(new Date(date_from).getTime() - 60 * 60 * 3 * 1000);
+		const dateUntil = await new Date(new Date(date_until).getTime() - 60 * 60 * 3 * 1000);
+
+		const dates = await freeAlg(business, services, dateFrom, dateUntil, 0);
 		if (dates.error) return res.status(404).json({ error: dates.error });
 
 		res.status(200).json({ dates });
