eebb64e36d0155ccfc04d2f7b2e14813f133997b
mahmoud taya <mahmoud12597@gmail.com>
2019-05-08 20:42:46 +0300

    Update free-alg.js

--- controllers/algs/free-alg.js
+++ controllers/algs/free-alg.js
@@ -762,9 +762,9 @@ return result;
                 });
             }
             days.push(new Day(chosendate,timeranges));
-            console.log(chosentimerange)
+            console.log("before"+chosentimerange)
             var tobook=  new time_range(new time(chosentimerange._start._hour,chosentimerange._start._minute) , new time(chosentimerange._end._hour,chosentimerange._end._minute) )
-            console.log(tobook)
+            console.log("after"+tobook)
             var day= new BinarySearchTree();
             tmpfree=days.shift();
             day.totree(tmpfree.Free);
