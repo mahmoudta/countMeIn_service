de6b4ee94574bd45a4f7539f6a786c4d34d00f38
mahmoud taya <mahmoud12597@gmail.com>
2019-05-26 03:02:32 +0300

    added smart-alg

--- controllers/algs/smart-alg.js
+++ controllers/algs/smart-alg.js
@@ -3,3 +3,10 @@ let app = new express();
 var Appointment = require('../../models/appointment');
 const isEmpty = require('lodash/isEmpty');
 const moment = require('moment');
+
+async function returnallappointmentsbythiscustomer(customerid){ 
+    const appointments = await Appointment.find({client_id: customerid})
+    return appointments;
+ }
+
+module.exports = {}
