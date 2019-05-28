2e93a872552a10f72ed96372273ecab1285f023d
nid.stud@gmail.com <Justfuckingdoit1>
2019-05-28 03:53:53 +0300

    REFACTOR

--- models/appointment.js
+++ models/appointment.js
@@ -1,32 +1,41 @@
-var mongoose = require('mongoose'),
-	appointment = new mongoose.Schema({
-		business_id: String,
-		client_id: String,
-		time: {
-			date: Date,
-			start: {
-				_hour: Number,
-				_minute: Number
-			},
-			end: {
-				_hour: Number,
-				_minute: Number
-			}
-		},
-		services: [ String ],
-		status: {
-			type: String,
-			enum: [ 'ready', 'inProgress', 'done', 'pendingClient', 'pendingBusiness', 'passed', 'canceled' ],
-			default: 'ready'
-		}
-		/*
+var mongoose = require("mongoose"),
+  appointment = new mongoose.Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId },
+    business_id: { type: mongoose.Schema.Types.ObjectId, ref: "Business" },
+    client_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    time: {
+      date: Date,
+      start: {
+        _hour: Number,
+        _minute: Number
+      },
+      end: {
+        _hour: Number,
+        _minute: Number
+      }
+    },
+    services: [{ type: mongoose.Schema.Types.ObjectId, ref: "Service" }],
+    status: {
+      type: String,
+      enum: [
+        "ready",
+        "inProgress",
+        "done",
+        "pendingClient",
+        "pendingBusiness",
+        "passed",
+        "canceled"
+      ],
+      default: "ready"
+    }
+    /*
 		feedback:{
 			clinet:
 			business:
 		}
 		*/
-	});
+  });
 
-var Appointment = mongoose.model('Appointment', appointment);
+var Appointment = mongoose.model("Appointment", appointment);
 
 module.exports = Appointment;
