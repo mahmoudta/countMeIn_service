30d1116c8409c4c1482fbe55f4a5ce38b2e7eb2e
nid.stud@gmail.com <Justfuckingdoit1>
2019-06-18 01:37:11 +0300

    CustomerReview

--- controllers/users.ctl.js
+++ controllers/users.ctl.js
@@ -19,18 +19,18 @@ const mongoose = require('mongoose');
 signInToken = (user, business_id = '') => {
 	return JWT.sign(
 		{
-			sub             : user._id,
-			isAdmin         : user.isAdmin,
-			profile         : user.profile,
-			isBusinessOwner : !isEmpty(business_id) > 0 ? true : false,
-			business_id     : business_id
+			sub: user._id,
+			isAdmin: user.isAdmin,
+			profile: user.profile,
+			isBusinessOwner: !isEmpty(business_id) > 0 ? true : false,
+			business_id: business_id
 		},
 		JWT_SECRET
 	);
 };
 
 module.exports = {
-	signUp                   : async (req, res, next) => {
+	signUp: async (req, res, next) => {
 		console.log('signUp Called!!');
 
 		const { email, password, first_name, last_name, imgUrl_, phone_ } = req.value.body;
@@ -39,20 +39,20 @@ module.exports = {
 			return res.status(403).json({ message: 'user already exist' });
 		}
 		const newUser = new Users({
-			_id     : new mongoose.Types.ObjectId(),
-			method  : 'local',
-			email   : email,
-			local   : {
-				password : password
+			_id: new mongoose.Types.ObjectId(),
+			method: 'local',
+			email: email,
+			local: {
+				password: password
 			},
 
-			profile : {
-				name   : {
-					first : first_name,
-					last  : last_name
+			profile: {
+				name: {
+					first: first_name,
+					last: last_name
 				},
-				imgUrl : imgUrl_,
-				phone  : phone_
+				imgUrl: imgUrl_,
+				phone: phone_
 			}
 		});
 		await newUser.save();
@@ -60,7 +60,7 @@ module.exports = {
 		res.status(200).json({ token });
 	},
 
-	signIn                   : async (req, res, next) => {
+	signIn: async (req, res, next) => {
 		console.log('signIn Called!!');
 		const business = await Businesses.findOne({ owner_id: req.user._id });
 		let token;
@@ -73,18 +73,18 @@ module.exports = {
 		res.status(200).json({ token });
 	},
 
-	googleOAuth              : (req, res, next) => {
+	googleOAuth: (req, res, next) => {
 		const token = signInToken(req.user);
 		res.status(200).json({ token });
 	},
 
-	secret                   : async (req, res, next) => {
+	secret: async (req, res, next) => {
 		console.log('secret Called!!');
 		// console.log(req.user);
 		// console.log(req.user.user);
 	},
 
-	test                     : async (req, res, next) => {
+	test: async (req, res, next) => {
 		//const date = new Date(2019, 5, 14);
 		//console.log(date);
 		// var momentdate = moment().add(1, 'days').format('l');
@@ -99,27 +99,27 @@ module.exports = {
 		//const test1 = await aftereditingbusnessworkinghours('5cedfa110a209a0eddbb2bbb', array);
 		//if (vvv) res.status(200).json({ vvv });
 	},
-	booktest                 : async (req, res, next) => {
+	booktest: async (req, res, next) => {
 		// console.log('book Test Here');
 		const test1 = await smart(
 			'5cedfa110a209a0eddbb2bbb',
-			[ '5cedf5813e3dad305192241e' ],
+			['5cedf5813e3dad305192241e'],
 			'5cedf3d20a209a0eddbb2bb2',
 			1
 		);
 		res.status(200).json({ test1 });
 	},
-	databasetest             : async (req, res, next) => {
+	databasetest: async (req, res, next) => {
 		console.log('database Test Here');
 		var date1 = await new Date(2019, 5, 12);
 		var date2 = await new Date(2019, 5, 12);
-		const test1 = await freeAlg('5cedfa110a209a0eddbb2bbb', [ '5cedf5813e3dad305192241e' ], date1, date2);
+		const test1 = await freeAlg('5cedfa110a209a0eddbb2bbb', ['5cedf5813e3dad305192241e'], date1, date2);
 		res.status(200).json({ test1 });
 	},
-	getUpcommingAppointments : async (req, res, next) => {
+	getUpcommingAppointments: async (req, res, next) => {
 		let ResArray = new Array();
 		const QueryRes = await Appointments.find({
-			client_id : req.user._id
+			client_id: req.user._id
 		}).populate('business_id', 'profile');
 		var promise = QueryRes.map(async (appointment, i) => {
 			const BusinessProfile = await Businesses.findById(appointment.business_id);
@@ -140,7 +140,7 @@ module.exports = {
 			let services = appointment.services;
 			let time = shour.toString() + ':' + sminute.toString();
 
-			ResArray = [ appointment.business_id._id, appointment._id, i + 1, BusinessName, time, thisdate, services ];
+			ResArray = [appointment.business_id._id, appointment._id, i + 1, BusinessName, time, thisdate, services];
 
 			//	ResArray.push(BusinessProfile.profile.name);
 			//	console.log(ResArray);
@@ -151,30 +151,30 @@ module.exports = {
 
 		res.json(FinalArray);
 	},
-	getFallowedBusinesses    : async (req, res, next) => {
+	getFallowedBusinesses: async (req, res, next) => {
 		const businesses = await Users.findOne({ _id: req.user._id }).populate('following', 'profile');
-		console.log(businesses.following);
+		//console.log("fallowed", businesses.following);
 		res.json({ following: businesses.following });
 	},
 
-	getAllBusinesses         : async (req, res, next) => {
+	getAllBusinesses: async (req, res, next) => {
 		const businesses = await Businesses.find({}, 'profile');
 		res.json({ businesses });
 	},
 
-	appendNotification       : async (req, res, next) => {
+	appendNotification: async (req, res, next) => {
 		const { title, type, my_business, appointment_id, status } = req.body;
 
 		if (!isEmpty(appointment_id)) {
 			const isAlreadyHere = await Users.find({
-				'notification.appointment_id' : appointment_id,
-				'notification.status'         : status
+				'notification.appointment_id': appointment_id,
+				'notification.status': status
 			});
 			if (isAlreadyHere) return res.status(200).json({ notifications: isAlreadyHere });
 		}
 		let update = {
-			$push : {
-				notification : { title, Type: type, my_business, appointment_id }
+			$push: {
+				notification: { title, Type: type, my_business, appointment_id }
 			}
 		};
 		const user = await Users.findOneAndUpdate({ _id: req.user._id }, update, { new: true });
--- models/business.js
+++ models/business.js
@@ -1,151 +1,151 @@
 var mongoose = require('mongoose'),
 	business = new mongoose.Schema({
-		_id               : { type: mongoose.Schema.Types.ObjectId },
-		Schedule_id       : String /*the Schedule id for this bussniess*/,
-		owner_id          : {
+		_id: { type: mongoose.Schema.Types.ObjectId },
+		Schedule_id: String /*the Schedule id for this bussniess*/,
+		owner_id: {
 			// type: String /* id of client that owns this bussniess*/,
-			required : true,
-			type     : mongoose.Schema.Types.ObjectId,
-			ref      : 'User'
+			required: true,
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User'
 		},
-		categories        : [
+		categories: [
 			{
-				type     : mongoose.Schema.Types.ObjectId,
-				required : true,
-				ref      : 'Category'
+				type: mongoose.Schema.Types.ObjectId,
+				required: true,
+				ref: 'Category'
 			},
 			{ _id: 0 }
 			//Array
 		],
-		services          : [
+		services: [
 			{
 				//services
-				service_id : {
-					type : mongoose.Schema.Types.ObjectId,
-					ref  : 'Service'
+				service_id: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: 'Service'
 				},
-				time       : {
-					type : Number,
-					max  : 120
+				time: {
+					type: Number,
+					max: 120
 				},
-				cost       : Number
+				cost: Number
 			},
 			{ _id: false }
 		],
-		break_time        : {
-			type    : Number,
-			default : 10
+		break_time: {
+			type: Number,
+			default: 10
 		},
-		working_hours     : [
+		working_hours: [
 			{
 				/* array which length = 7 (week)*/
-				day    : {
-					type : String
+				day: {
+					type: String
 					// enum: [ 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday' ]
 				},
-				opened : Boolean,
-				from   : {
-					type : Date
+				opened: Boolean,
+				from: {
+					type: Date
 				},
-				until  : {
-					type : Date
+				until: {
+					type: Date
 				},
-				break  : {
-					isBreak : Boolean,
-					from    : Date,
-					until   : Date
+				break: {
+					isBreak: Boolean,
+					from: Date,
+					until: Date
 				}
 			},
 			{ _id: false }
 		],
-		schedule_settings : {
+		schedule_settings: {
 			/* */
-			customers_exp             : {
-				type    : Boolean,
-				default : true
+			customers_exp: {
+				type: Boolean,
+				default: true
 			},
-			continuity                : {
-				type    : Number,
-				default : 3,
-				max     : 10,
-				min     : 0
+			continuity: {
+				type: Number,
+				default: 3,
+				max: 10,
+				min: 0
 			},
-			distrbuted_time           : {
-				type    : Number,
-				default : 3,
-				max     : 20,
-				min     : 1
+			distrbuted_time: {
+				type: Number,
+				default: 3,
+				max: 20,
+				min: 1
 			},
-			days_calculate_length     : {
-				type    : Number,
-				default : 7,
-				min     : 2,
-				max     : 30
+			days_calculate_length: {
+				type: Number,
+				default: 7,
+				min: 2,
+				max: 30
 			},
-			max_working_days_response : {
-				type    : Number,
-				default : 7,
-				min     : 1,
-				max     : 30
+			max_working_days_response: {
+				type: Number,
+				default: 7,
+				min: 1,
+				max: 30
 			},
-			experiance_rule           : {
-				type    : Number,
-				default : 1,
-				min     : 1,
-				max     : 5
+			experiance_rule: {
+				type: Number,
+				default: 1,
+				min: 1,
+				max: 5
 			},
-			customer_prefered_period  : {
-				type    : Number,
-				default : 6,
-				min     : 1,
-				max     : 9
+			customer_prefered_period: {
+				type: Number,
+				default: 6,
+				min: 1,
+				max: 9
 			}
 		},
-		profile           : {
-			name        : {
-				type     : String,
-				required : true
+		profile: {
+			name: {
+				type: String,
+				required: true
 			},
-			phone       : {
-				type     : String,
-				required : true,
-				index    : { unique: true }
+			phone: {
+				type: String,
+				required: true,
+				index: { unique: true }
 			},
-			description : String,
+			description: String,
 
-			img         : String,
+			img: String,
 
-			rating      : Number,
+			rating: Number,
 
-			location    : {
-				street      : String,
-				city        : String,
-				building    : Number,
-				postal_code : Number
+			location: {
+				street: String,
+				city: String,
+				building: Number,
+				postal_code: Number
 			},
-			views       : {
-				type    : Number,
-				default : 0
+			views: {
+				type: Number,
+				default: 0
 			}
 		} /*end of profile*/,
-		style_id          : String /* id for style document*/,
+		style_id: String /* id for style document*/,
 		//
-		customers         : [
+		customers: [
 			{
-				customer_id : {
-					type : mongoose.Schema.Types.ObjectId,
-					ref  : 'User'
+				customer_id: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: 'User'
 				},
-				isFollower  : Boolean,
-				experiance  : {
-					type    : Number,
-					default : 0
+				isFollower: Boolean,
+				experiance: {
+					type: Number,
+					default: 0
 				}
 			}
 		] /* array of Clients_ids */
 	});
 
-business.methods.createTime = async function(time) {
+business.methods.createTime = async function (time) {
 	try {
 		let splitter = time.split(':');
 		let date = new Date();
@@ -157,8 +157,8 @@ business.methods.createTime = async function(time) {
 	}
 };
 
-business.post('find', async function(doc, next) {
-	console.log(this);
+business.post('find', async function (doc, next) {
+	//console.log(this);
 	// if (this.method != 'local') {
 	// 	next();
 	// }
