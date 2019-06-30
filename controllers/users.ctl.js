236e2ebfc7e02d41cd4d3f7d6575f7a67cac602b
mahmoud taya <mahmoud12597@gmail.com>
2019-06-30 03:34:16 +0300

    fixed a problime in case of first appointment for the business

--- controllers/users.ctl.js
+++ controllers/users.ctl.js
@@ -20,18 +20,18 @@ const mongoose = require('mongoose');
 signInToken = (user, business_id = '') => {
 	return JWT.sign(
 		{
-			sub: user._id,
-			isAdmin: user.isAdmin,
-			profile: user.profile,
-			isBusinessOwner: !isEmpty(business_id) > 0 ? true : false,
-			business_id: business_id
+			sub             : user._id,
+			isAdmin         : user.isAdmin,
+			profile         : user.profile,
+			isBusinessOwner : !isEmpty(business_id) > 0 ? true : false,
+			business_id     : business_id
 		},
 		JWT_SECRET
 	);
 };
 
 module.exports = {
-	signUp: async (req, res, next) => {
+	signUp                   : async (req, res, next) => {
 		console.log('signUp Called!!');
 
 		const { email, password, first_name, last_name, imgUrl_, phone_ } = req.value.body;
@@ -40,20 +40,20 @@ module.exports = {
 			return res.status(403).json({ message: 'user already exist' });
 		}
 		const newUser = new Users({
-			_id: new mongoose.Types.ObjectId(),
-			method: 'local',
-			email: email,
-			local: {
-				password: password
+			_id     : new mongoose.Types.ObjectId(),
+			method  : 'local',
+			email   : email,
+			local   : {
+				password : password
 			},
 
-			profile: {
-				name: {
-					first: first_name,
-					last: last_name
+			profile : {
+				name   : {
+					first : first_name,
+					last  : last_name
 				},
-				imgUrl: imgUrl_,
-				phone: phone_
+				imgUrl : imgUrl_,
+				phone  : phone_
 			}
 		});
 		await newUser.save();
@@ -61,7 +61,7 @@ module.exports = {
 		res.status(200).json({ token });
 	},
 
-	signIn: async (req, res, next) => {
+	signIn                   : async (req, res, next) => {
 		console.log('signIn Called!!');
 		const business = await Businesses.findOne({ owner_id: req.user._id });
 		let token;
@@ -74,18 +74,18 @@ module.exports = {
 		res.status(200).json({ token });
 	},
 
-	googleOAuth: (req, res, next) => {
+	googleOAuth              : (req, res, next) => {
 		const token = signInToken(req.user);
 		res.status(200).json({ token });
 	},
 
-	secret: async (req, res, next) => {
+	secret                   : async (req, res, next) => {
 		console.log('secret Called!!');
 		// console.log(req.user);
 		// console.log(req.user.user);
 	},
 
-	test: async (req, res, next) => {
+	test                     : async (req, res, next) => {
 		var momentdate = moment().add(3, 'days').format('l');
 		var date = new Date(momentdate);
 		console.log(date);
@@ -99,24 +99,24 @@ module.exports = {
 		);
 		res.status(200).json({ test1 });
 	},
-	booktest: async (req, res, next) => {
+	booktest                 : async (req, res, next) => {
 		// console.log('book Test Here');
 		const test1 = await smart(
-			'5cee375f0d1aca9031f57708',
-			['5cedf8f83e3dad3051922424', '5cedf9bb3e3dad3051922426'],
-			'5cedf3c20a209a0eddbb2bb1',
+			'5d11ae5eae0b134a3721bd7c',
+			[ '5cedf5403e3dad305192241d', '5cedf4c13e3dad305192241a' ],
+			'5d07d11285f0e1296140f4d5',
 			0
 		);
 		res.status(200).json({ test1 });
 	},
-	databasetest: async (req, res, next) => {
+	databasetest             : async (req, res, next) => {
 		console.log('database Test Here');
 		var date1 = await new Date(2019, 5, 12);
 		var date2 = await new Date(2019, 5, 12);
-		const test1 = await freeAlg('5cedfa110a209a0eddbb2bbb', ['5cedf5813e3dad305192241e'], date1, date2);
+		const test1 = await freeAlg('5cedfa110a209a0eddbb2bbb', [ '5cedf5813e3dad305192241e' ], date1, date2);
 		res.status(200).json({ test1 });
 	},
-	getUpcommingAppointments: async (req, res, next) => {
+	getUpcommingAppointments : async (req, res, next) => {
 		let ResArray = new Array();
 		//let services = new Array();
 		//var today = moment(moment(new Date()).format('l')) //new Date();
@@ -125,13 +125,14 @@ module.exports = {
 		//yesterday.setDate(yesterday.getDate() - 1)
 		console.log('user', req.user._id);
 		const QueryRes = await Appointments.find({
-			client_id: req.user._id,
-			'time.date': { $gte: today },
-			status: { $in: ['inProgress', 'ready'] }
-		}).sort([['time.date', 1], ['time.start._hour', 1], ['time.start._minute', 1]]).
-			populate({
-				path: 'services',
-				populate: { path: 'services' }
+			client_id   : req.user._id,
+			'time.date' : { $gte: today },
+			status      : { $in: [ 'inProgress', 'ready' ] }
+		})
+			.sort([ [ 'time.date', 1 ], [ 'time.start._hour', 1 ], [ 'time.start._minute', 1 ] ])
+			.populate({
+				path     : 'services',
+				populate : { path: 'services' }
 			})
 			.populate('business_id');
 
@@ -161,7 +162,7 @@ module.exports = {
 				})
 			);
 			let time = shour.toString() + ':' + sminute.toString() + '-' + ehour.toString() + ':' + eminute.toString();
-			ResArray = [appointment.business_id._id, appointment._id, i + 1, BusinessName, time, thisdate, services];
+			ResArray = [ appointment.business_id._id, appointment._id, i + 1, BusinessName, time, thisdate, services ];
 
 			//	ResArray.push(BusinessProfile.profile.name);
 			//	console.log(ResArray);
@@ -172,17 +173,17 @@ module.exports = {
 
 		res.json(FinalArray);
 	},
-	getFallowedBusinesses: async (req, res, next) => {
+	getFallowedBusinesses    : async (req, res, next) => {
 		const businesses = await Users.findOne({ _id: req.user._id }).populate('following', 'profile');
 		res.json({ following: businesses.following });
 	},
 
-	getAllBusinesses: async (req, res, next) => {
+	getAllBusinesses         : async (req, res, next) => {
 		const businesses = await Businesses.find({}, 'profile');
 		res.json({ businesses });
 	},
 
-	setReminder: async (req, res, next) => {
+	setReminder              : async (req, res, next) => {
 		const { businessId, customerId, services, days, repeat } = req.body;
 		//var todayDate = new Date();
 		const newDate = moment(moment(new Date()).format('l')).add(Number(days), 'days');
@@ -199,13 +200,13 @@ module.exports = {
 		//console.log(thecustomer_id)
 
 		let update = {
-			$push: {
-				reminders: {
-					business_id: thebusiness_id,
-					services: theservices,
-					days: days,
-					date_to: newDate,
-					repeat: repeat
+			$push : {
+				reminders : {
+					business_id : thebusiness_id,
+					services    : theservices,
+					days        : days,
+					date_to     : newDate,
+					repeat      : repeat
 				}
 			}
 		};
@@ -213,7 +214,7 @@ module.exports = {
 			{ _id: customerId, 'reminders.business_id': { $ne: thebusiness_id } },
 			update,
 			{
-				upsert: true
+				upsert : true
 			}
 		);
 
@@ -222,19 +223,19 @@ module.exports = {
 		res.status(200).json({ result: 'aLready has reminder' });
 	},
 
-	appendNotification: async (req, res, next) => {
+	appendNotification       : async (req, res, next) => {
 		const { title, type, my_business, appointment_id, status } = req.body;
 
 		if (!isEmpty(appointment_id)) {
 			const isAlreadyHere = await Users.find({
-				'notification.appointment_id': appointment_id,
-				'notification.status': status
+				'notification.appointment_id' : appointment_id,
+				'notification.status'         : status
 			});
 			if (isAlreadyHere) return res.status(200).json({ notifications: isAlreadyHere });
 		}
 		let update = {
-			$push: {
-				notification: { title, Type: type, my_business, appointment_id }
+			$push : {
+				notification : { title, Type: type, my_business, appointment_id }
 			}
 		};
 		const user = await Users.findOneAndUpdate({ _id: req.user._id }, update, { new: true });
@@ -243,7 +244,7 @@ module.exports = {
 		res.status(200).json({ notifications: user.notification });
 	}
 };
-Date.prototype.addDays = function (days) {
+Date.prototype.addDays = function(days) {
 	var date = new Date(this.valueOf());
 	date.setDate(date.getDate() + days);
 	return date;
